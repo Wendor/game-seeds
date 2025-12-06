866b8730476ed1e8032c3f59f67e505a7bc7cc3b
Anton Solomonov <a.solomonov.gc@gmail.com>
2025-12-06 15:55:59 +0700

    some

--- src/scenes/Game.vue
+++ src/scenes/Game.vue
@@ -86,7 +86,7 @@
         <svg v-if="isBotActive" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" stroke="none"><rect x="6" y="6" width="12" height="12" rx="2" /></svg>
         <svg v-else xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="11" width="18" height="10" rx="2" /><circle cx="12" cy="5" r="2" /><path d="M12 7v4" /><line x1="8" y1="16" x2="8" y2="16" /><line x1="16" y1="16" x2="16" y2="16" /></svg>
       </button>
-      <button @click="showRestartModal = true" class="btn btn-danger btn-icon" :title="t('game.restart')">↺</button>
+      <button @click="showRestartModal = true" class="btn btn-secondary btn-icon" :title="t('game.restart')">↺</button>
     </footer>
   </section>
 </template>
